8fa084b55bdb663a9bb058d7a23da6b7387b4271
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-20 12:26:33 +0200

    v2 :

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -7,10 +7,21 @@
 		return {
 			restrict: 'E',
 			template: `<img ng-repeat="i in [1, 2, 3, 4, 5]" 
-							src="./ors-star/img/white_star.png"
-							alt="Etoile blanche">`
+							ng-src="{{$ctrl.getImage(i)}}"
+							alt="Etoile blanche">`,
+			controller: function OrsStarCtrl() {
+				this.getImage = function (i) {
+					if (i > 3) {
+						return './ors-star/img/white_star.png';
+					}
+					return './ors-star/img/yellow_star.png';
+				}
+
+			}, 
+			controllerAs: '$ctrl'
 		};
 	});
 
     
+
 })();
