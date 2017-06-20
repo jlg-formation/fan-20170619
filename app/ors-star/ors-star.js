d4c601bfaf741dda8a674732642d2a674d70793d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-20 12:35:36 +0200

    v3 : sans watcher

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,17 +6,27 @@
 	app.directive('orsStar', function () {
 		return {
 			restrict: 'E',
-			template: `<img ng-repeat="i in [1, 2, 3, 4, 5]" 
-							ng-src="{{$ctrl.getImage(i)}}"
-							alt="Etoile blanche">`,
-			controller: function OrsStarCtrl() {
-				this.getImage = function (i) {
-					if (i > 3) {
-						return './ors-star/img/white_star.png';
-					}
-					return './ors-star/img/yellow_star.png';
+			// template: `<img ng-repeat="i in [1, 2, 3, 4, 5]" 
+			// 				ng-src="{{$ctrl.getImage(i)}}"
+			// 				alt="Etoile blanche">`,
+
+			controller: function OrsStarCtrl($element) {
+				'ngInject';
+				// this.getImage = function (i) {
+				// 	if (i > 3) {
+				// 		return './ors-star/img/white_star.png';
+				// 	}
+				// 	return './ors-star/img/yellow_star.png';
+				// }
+				let html = '';
+				for (var i = 0; i < 3; i++) {
+					html += '<img src="./ors-star/img/yellow_star.png">';
+				}
+				for (var i = 3; i < 5; i++) {
+					html += '<img src="./ors-star/img/white_star.png">';
 				}
 
+				$element.html(html);
 			}, 
 			controllerAs: '$ctrl'
 		};
