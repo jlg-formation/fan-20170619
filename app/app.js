cfa065f56e216fd9b7c00b16f05556511d060b46
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-20 17:13:40 +0200

    webpack

--- app/app.js
+++ app/app.js
@@ -1,26 +1,28 @@
-(function () {
-	'use strict';
+import 'angular';
 
-	var app = angular.module('main', ['ors-star']);
+import './ors-star/ors-star.js';
+import 'angular/angular-csp.css';
+import './style.css';
 
-	app.directive('orsHeader', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-header.html'
-		};
-	});
+var app = angular.module('main', ['ors-star']);
 
-    app.directive('orsBody', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-body.html'
-		};
-	});
+app.directive('orsHeader', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'tmpl/ors-header.html'
+    };
+});
 
-    app.directive('orsFooter', function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-footer.html'
-		};
-	});
-})();
+app.directive('orsBody', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'tmpl/ors-body.html'
+    };
+});
+
+app.directive('orsFooter', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'tmpl/ors-footer.html'
+    };
+});
