7b7aa33b303deaa024fa122e3b513508247fe0f7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-21 15:20:30 +0200

    EBO : promise http, appel 6 services.

--- app/ors-route/ProductCtrl.js
+++ app/ors-route/ProductCtrl.js
@@ -1,6 +1,34 @@
 export class ProductCtrl {
+
+    // static get $inject(){ return ['$http']; }
+
+    constructor($http, $q) {
+        'ngInject';
+        this.$http = $http;
+        this.$q = $q;
+    }
+
     start() {
         console.log('Start !!');
+        this.$http.get('../ws/s1').then((response) => {
+            console.log('response', response);
+            return this.$q.all([
+                this.$http.get('../ws/s2').then((response) => {
+                    console.log('response', response);
+                    return this.$http.get('../ws/s5');
+                }),
+                this.$http.get('../ws/s3'),
+                this.$http.get('../ws/s4')
+            ]);
+        }).then((responses) => {
+            console.log('responses', responses);
+            return this.$http.get('../ws/s6');
+        }).then((response) => {
+            console.log('response', response);
+        }).catch((error) => {
+            console.error('error', error);
+        });
+
     }
 
 }
